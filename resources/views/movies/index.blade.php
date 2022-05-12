4f7482ea25c45b987b1e61f8f78f59c506c1d627
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-05-12 16:27:08 +0200

    add a simple footer

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,25 +5,23 @@
 @section('main-content')
 
 <section class="container">
-    <div class="row g-3">
+    <div class="row row-cols-1 row-cols-md-2 g-4">
         @foreach ($movies as $movie)
-            <div class="col-3 ">
-                <div class="card">
-                    <img src="https://picsum.photos/200/300" class="card-img-top" alt="random picture">
-                    <div class="card-body">
-                        <h5 class="card-title"> Title :{{$movie->title}} </h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa est, quisquam sint et doloremque nobis quod recusandae ducimus excepturi obcaecati illum saepe sequi libero nihil, voluptas ipsam enim aut nostrum.</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"> Original name: {{$movie->original_title}}</li>
-                        <li class="list-group-item"> Nationality: {{$movie->nationality}}</li>
-                        <li class="list-group-item"> Date: {{$movie->date}}</li>
-                    </ul>
-                    <div class="card-body">
-                        <p href="#" class="card-link"> Imdb Rate: {{$movie->vote}}</p>
+                <div class="col">
+                    <div class="card">
+                        <img src="https://picsum.photos/200/200" class="card-img-top" alt="random picture">
+                        <div class="card-body">
+                            <h5 class="card-title">Title: {{$movie->title}}</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis assumenda, rerum quasi ab voluptas consequatur, iusto amet rem accusantium facere ducimus cum numquam maxime ex molestiae odio id, fuga exercitationem.</p>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item"> Original name: {{$movie->original_title}}</li>
+                                <li class="list-group-item"> Nationality: {{$movie->nationality}}</li>
+                                <li class="list-group-item"> Date: {{$movie->date}}</li>
+                            </ul>
+                            <p class="card-text text-warning"> Imdb Rate: {{$movie->vote}}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
         @endforeach
     </div>
 </section>
